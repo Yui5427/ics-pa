7e532375f6563d5bafe56883092f465d0cfcb2ca
tracer-ics2022 <tracer@njuics.org>
2023-07-13 21:32:01 +0800

    >  compile NEMU 211220000 张三 Linux rez 6.2.0-24-generic #24-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 16 12:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:32:01 up 6 days,  2:17,  2 users,  load average: 2.43, 2.28, 2.40

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -110,10 +110,11 @@ static int cmd_x(char *args) {
   
   for(;n>0;n--){
     printf("0x%x: ", expr_value);
-    printf("0x%08x\n",vaddr_read(expr_value, 4));
+    printf("0x%08x",vaddr_read(expr_value, 4));
     for(int i = 0; i < 4; i++){
       printf("%02x ", vaddr_read(expr_value + i, 1));
     }
+    printf("\n");
     expr_value += 4;
   }
   
