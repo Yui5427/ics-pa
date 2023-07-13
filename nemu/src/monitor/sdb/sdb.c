b4c3487c617408104b5b4c6be80c13afc222de43
tracer-ics2022 <tracer@njuics.org>
2023-07-13 21:36:21 +0800

    >  compile NEMU 211220000 张三 Linux rez 6.2.0-24-generic #24-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun 16 12:03:50 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:36:21 up 6 days,  2:21,  2 users,  load average: 2.30, 2.41, 2.43

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -106,7 +106,7 @@ static int cmd_x(char *args) {
   int expr_value = myAtoi(expr);
 
   //printf("%d, %s, %x\n", n, expr, expr_value);
-  printf("Address\t\t\n");
+  printf("  Address\t\t  Dword block\t\t Byte sequence\n");
 
   for(;n>0;n--){
     printf("0x%x:\t", expr_value);
